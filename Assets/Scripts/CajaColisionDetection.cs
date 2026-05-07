e5b9b9c6f167df59b0adbaa693475d51fc3067bc
LeonelGunther <49980496@est.ort.edu.ar>
2026-05-07 11:43:24 -0300

    Volver a arriba cuando toca el piso

---
+++ Assets/Scripts/CajaColisionDetection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaColisionDetection : MonoBehaviour
+{
+    public CajaMovement cajaMovementscript;
+
+    void Start()
+    {
+        cajaMovementscript = GetComponent<CajaMovement>();
+
+    }
+     void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Piso"))
+        {
+           cajaMovementscript.MmoveBoxToRamdomPosition();
+        }
+    }
+}
--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CajaMovement : MonoBehaviour
 {
         public float speed = 2f;
+            float initialYValue;
     // Start is called before the first frame update
     void Start()
     {
-        
+                initialYValue = transform.position.y;
     }
 
     // Update is called once per frame
@@ -16,5 +17,11 @@ public class CajaMovement : MonoBehaviour
     {
         transform.Translate(0,-speed * Time.deltaTime,0);
 
+    }
+    public void MmoveBoxToRamdomPosition()
+    {
+        Vector3 newPosition =  transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition;
     }
 }
